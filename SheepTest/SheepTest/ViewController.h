d36ed100c97355e2f838c557c8c211d65e97daeb
ElonZhang <ElonZhang@163.com>
2015-12-01 10:05:58 +0800

    test-test

---
+++ SheepTest/SheepTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SheepTest
+//
+//  Created by tarena04 on 15/12/1.
+//  Copyright © 2015年 sheep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SheepTest/SheepTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SheepTest
+//
+//  Created by tarena04 on 15/12/1.
+//  Copyright © 2015年 sheep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
